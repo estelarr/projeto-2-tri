b0038e1908908df6b858b577e1712462a7583e91
Estelar <166717236+estelarr@users.noreply.github.com>
2025-06-16 07:43:01 -0300

    Add files via upload

---
+++ el/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Conhecimento Gerais',
+    'Que país tem o formato de uma bota?',
+    'Itália'
+)
